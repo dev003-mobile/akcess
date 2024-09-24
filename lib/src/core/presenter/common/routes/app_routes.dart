7e982b40b44e409d599a9dcb910daca89b7fd5b7
Wagner Coche <dev003.mobile@gmail.com>
2024-09-24 12:59:12 +0100

    🌈 style: app logo configured

--- lib/src/core/presenter/common/routes/app_routes.dart
+++ lib/src/core/presenter/common/routes/app_routes.dart
@@ -1,7 +1,10 @@
 import 'package:get/get.dart';
 
+import 'app_name_route.dart';
+import '../../../../module/initial_module/screens/splash_screen/splash_screen.dart';
+
 class AppRoutes {
   static List<GetPage<dynamic>> routes = <GetPage<dynamic>>[
-    // GetPage(name: AppNameRoute.splashScreen, page: () => SplashScreen()),
+    GetPage(name: AppNameRoute.splashScreen, page: () => const SplashScreen()),
   ];
 }
